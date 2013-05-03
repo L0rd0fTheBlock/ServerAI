77d11bb24e7419777127a1be7d7b9c97c92f598c
jamie <jamie@192.168.0.14>
2013-05-03 01:12:14 +0100

    Created Class to handle all teleport functions

---
+++ src/me/jamiemac262/ServerAIReWrite/function/Teleport.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.jamiemac262.ServerAIReWrite.function;
+
+import java.util.Arrays;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author jamie
+ */
+public class Teleport {
+
+    public void tp(Player target, Player victim) {
+
+        target.teleport(victim);
+    }
+    public void toMe(Player victim, Player me){
+        victim.teleport(me);
+    }
+    public void meTo(Player victim, Player me){
+        me.teleport(victim);
+    }
+}
