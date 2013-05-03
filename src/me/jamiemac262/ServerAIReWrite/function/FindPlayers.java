3b9a5eaebb6b81e72ceb733ab046d286bd1c8a53
jamie <jamie@192.168.0.14>
2013-05-03 01:12:50 +0100

    added function to find multiple players

--- src/me/jamiemac262/ServerAIReWrite/function/FindPlayers.java
+++ src/me/jamiemac262/ServerAIReWrite/function/FindPlayers.java
@@ -1,4 +1,3 @@
-
 package me.jamiemac262.ServerAIReWrite.function;
 
 import java.util.ArrayList;
@@ -12,51 +11,85 @@ import org.bukkit.entity.Player;
  * @author jamie
  */
 public class FindPlayers {
-        public static Player findPlayerInArray(String[] playernames) {
+
+    public static Player findPlayerInArray(String[] playernames) {
         for (int i = 0; i < playernames.length; i++) {
             String string = playernames[i];
             ServerAI.logger.log(Level.INFO, string);
         }
         Player foundPlayer = null;
         Player[] player = Bukkit.getOnlinePlayers();
-        
-        
-        
-        
+
+
+
+
         for (int i = 0; i < playernames.length; i++) {
             for (int j = 0; j < player.length; j++) {
                 String play = player[j].getPlayerListName().toLowerCase();
-                if(play.equals(playernames[i])){
-                foundPlayer = player[j];
-                }                
-            }          
+                if (play.equals(playernames[i])) {
+                    foundPlayer = player[j];
+                }
+            }
         }
-        
+
         return foundPlayer;
     }
 
-     public static Player findPlayerInArray(String[] playernames, Player p) {
+    public static Player findPlayerInArray(String[] playernames, Player p) {
         Player foundPlayer = null;
         Player[] player = Bukkit.getOnlinePlayers();
-        
+
         ArrayList chat = new ArrayList();
         for (int i = 0; i < playernames.length; i++) {
             chat.add(playernames[i]);
         }
-        
-      for (int i = 0; i < playernames.length; i++) {
+
+        for (int i = 0; i < playernames.length; i++) {
             for (int j = 0; j < player.length; j++) {
                 String play = player[j].getPlayerListName().toLowerCase();
-                if(play.equals(playernames[i])){
-                foundPlayer = player[j];
+                if (play.equals(playernames[i])) {
+                    foundPlayer = player[j];
                 }
-                if(foundPlayer == null && chat.contains("me")){
-                foundPlayer = p;
+                if (foundPlayer == null && chat.contains("me")) {
+                    foundPlayer = p;
                 }
-                
-            }          
+
+            }
         }
-        
+
+        return foundPlayer;
+    }
+
+    public static Player[] findPlayersInArray(String[] playernames, Player p) {
+        Player[] foundPlayer = null;
+        Player[] player = Bukkit.getOnlinePlayers();
+
+        ArrayList chat = new ArrayList();
+        for (int i = 0; i < playernames.length; i++) {
+            chat.add(playernames[i]);
+        }
+        for (int i = 0; i < playernames.length; i++) {
+            for (int j = 0; j < player.length; j++) {
+                String play = player[j].getPlayerListName().toLowerCase();
+                if (foundPlayer[0] !=null) {
+                    if (play.equals(playernames[i])) {
+                        foundPlayer[0] = player[j];
+                    }
+                    if (foundPlayer == null && chat.contains("me")) {
+                        foundPlayer[0] = p;
+                    }
+                }else{
+                if (play.equals(playernames[i]) && !foundPlayer[0].getPlayerListName().toLowerCase().equals(playernames[i])) {
+                        foundPlayer[1] = player[j];
+                    }
+                    if (foundPlayer[1] == null && chat.contains("me")) {
+                        foundPlayer[1] = p;
+                    }
+                }
+
+            }
+
+        }
         return foundPlayer;
     }
 }
